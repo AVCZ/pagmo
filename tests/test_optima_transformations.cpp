597abafa8aa67242b32d67aefd024d888d6192a6
Adam Vyskovsky <adamvyskovsky@gmail.com>
2015-03-10 20:01:54 +0100

    Fix the test suite for meta-problems

--- tests/test_optima_transformations.cpp
+++ tests/test_optima_transformations.cpp
@@ -41,45 +41,100 @@ bool is_eq(const decision_vector & x1, const decision_vector & x2){
 	return true;
 }
 
-int test_optima_transformations()
+// Going to test the code on the Himmelblau function,
+// see http://en.wikipedia.org/wiki/Himmelblau%27s_function
+// The four minima of the function in the range [-6,6]x[-6,6] are:
+// (x0,y0) = (3.0, 2.0)
+// (x1,y1) = (-2.805118,3.131312)
+// (x2,y2) = (-3.779310,-3.283186)
+// (x3,y3) = (3.584428,-1.848126)
+
+// Test Himmelblau function minima in the bounding box [-6,6]:
+const std::vector<decision_vector> best_x = {
+	{3.0, 2.0},
+	{-2.805118,3.131312},
+	{-3.779310,-3.283186},
+	{3.584428,-1.848126},
+};
+
+int test_shifted()
 {
-	// Going to test the code on the Himmelblau function,
-	// see http://en.wikipedia.org/wiki/Himmelblau%27s_function
-	// The four minima of the function in the range [-6,6]x[-6,6] are:
-	// (x0,y0) = (3.0, 2.0)
-	// (x1,y1) = (-2.805118,3.131312)
-	// (x2,y2) = (-3.779310,-3.283186)
-	// (x3,y3) = (3.584428,-1.848126)
-
 	problem::himmelblau prob;
-	// Himmelblau function minima in the bounding box [-6,6]:
-	const std::vector<decision_vector> best_x = prob.get_best_x();
+	problem::shifted shifted_prob(prob, {100,50});
+	// retrieve the shifted minima
+	const std::vector<decision_vector> shifted_minima = shifted_prob.get_best_x();
 
-	// ------------------------- Shifting --------------------------
-	std::vector<double> shift_vec1 = { 1., 1. };
-	problem::shifted shifted_prob1(prob, shift_vec1);
-	// Previous function minima shifted by vector (1,1):
-	const std::vector<decision_vector> shif1_best_x = shifted_prob1.get_best_x();
-
-	std::vector<double> shift_vec2 = { -2., 0. };
-	problem::shifted shifted_prob2(shifted_prob1, shift_vec2);
-	// Previous function minima shifted by vector (-2,0):
-	const std::vector<decision_vector> shif2_best_x = shifted_prob2.get_best_x();
-
-	std::vector<double> shift_vec3 = { 1., -1. };
-	problem::shifted shifted_prob3(shifted_prob2, shift_vec3);
-	// "Previous function minima shifted by vector (1,-1) (which is the original problem):
-	const std::vector<decision_vector> shif3_best_x = shifted_prob3.get_best_x();
-
+	// just check that we have actualy moved (somewhere)
+	bool all_equal = true;
 	for (unsigned int i = 0; i < best_x.size(); ++i) {
-		if (!is_eq(best_x[i],shif3_best_x[i])) return 1;
+		if (!is_eq(best_x[i],shifted_minima[i])) all_equal = false;
 	}
-	// -------------------------------------------------------------
+	if (all_equal) return 1;
 
-	// ------------------------- Normalization ---------------------
+
+	// Perform a number of shifts that walk around and get back to the origin in the end.
+	// It is possible to add any combination of translations satisfying such a condition.
+	const std::vector<std::vector<double>> shifts = {
+		{1., 1.},
+		{-2., 0.},
+		{1., -1.},
+
+		{1., 1.},
+		{-2., 0.},
+		{1., -1.},
+	};
+
+	// test the zero shift
+	problem::shifted zero_shifted_prob(prob, {0,0});
+	const std::vector<decision_vector> zero_shifted_minima = zero_shifted_prob.get_best_x();
+	for (unsigned int i = 0; i < best_x.size(); ++i) {
+		if (!is_eq(best_x[i],zero_shifted_minima[i])) return 1;
+	}
+
+	std::vector<problem::shifted> problems;
+	problems.push_back(zero_shifted_prob);
+
+	for (unsigned int i = 0; i < shifts.size(); ++i)
+		problems.push_back(problem::shifted(problems.back(), shifts[i]));
+
+	std::vector<std::vector<decision_vector>> expected_shifted_minima;
+	expected_shifted_minima.push_back(zero_shifted_prob.get_best_x());
+
+	for (unsigned int i = 0; i < shifts.size(); ++i) {
+		expected_shifted_minima.push_back({
+				{expected_shifted_minima.back()[0][0] + shifts[i][0], expected_shifted_minima.back()[0][1] + shifts[i][1]},
+				{expected_shifted_minima.back()[1][0] + shifts[i][0], expected_shifted_minima.back()[1][1] + shifts[i][1]},
+				{expected_shifted_minima.back()[2][0] + shifts[i][0], expected_shifted_minima.back()[2][1] + shifts[i][1]},
+				{expected_shifted_minima.back()[3][0] + shifts[i][0], expected_shifted_minima.back()[3][1] + shifts[i][1]},
+		});
+	}
+
+	for (unsigned int i = 0; i < problems.size(); ++i) {
+		const std::vector<decision_vector> shifted_best_x = problems[i].get_best_x();
+		for (unsigned int j = 0; j < shifted_best_x.size(); ++j) {
+			if (!is_eq(expected_shifted_minima[i][j],shifted_best_x[j])) return 1;
+		}
+	}
+
+	// Now check that we have actually returned to the origin back again.
+	for (unsigned int i = 0; i < problems.size(); ++i) {
+		if (!is_eq(expected_shifted_minima[0][i],expected_shifted_minima.back()[i])) return 1;
+	}
+
+	return 0;
+}
+
+int test_rotated()
+{
+	// tak the original Himmelblau function
+	problem::himmelblau prob;
+	// and normalize it
 	problem::normalized normalized_prob(prob);
-	// Normalizing bounds (to [-1,1]x[-1,1]) of the Himmelblau function and the corresponding transformed minima are:
-	const std::vector<decision_vector> norm_best_x = normalized_prob.get_best_x();
+
+	const std::vector<std::vector<double>> rot_id_mat = {{1.,0.},{0.,1.}}; // identity matrix
+	problem::rotated rotated_id_prob(normalized_prob,rot_id_mat);
+	// Normalized Himmelblau rotated by the identity matrix and the corresponding minima are:
+	const std::vector<decision_vector> rot_id_best_x = rotated_id_prob.get_best_x();
 
 	const std::vector<decision_vector> expected_normalized_minima = {
 		{best_x[0][0] / 6, best_x[0][1] / 6},
@@ -88,18 +143,7 @@ int test_optima_transformations()
 		{best_x[3][0] / 6, best_x[3][1] / 6},
 	};
 
-	for (unsigned int i = 0; i < norm_best_x.size(); ++i) {
-		if (!is_eq(expected_normalized_minima[i], norm_best_x[i])) return 1;
-	}
-	// -------------------------------------------------------------
-
-	// ------------------------- Rotation --------------------------
-	std::vector<std::vector<double>> rot_id_mat = {{1.,0.},{0.,1.}}; // identity matrix
-	problem::rotated rotated_id_prob(normalized_prob,rot_id_mat);
-	// Normalized Himmelblau rotated by the identity matrix and the corresponding minima are:
-	const std::vector<decision_vector> rot_id_best_x = rotated_id_prob.get_best_x();
-
-	for (unsigned int i = 0; i < norm_best_x.size(); ++i) {
+	for (unsigned int i = 0; i < expected_normalized_minima.size(); ++i) {
 		if (!is_eq(expected_normalized_minima[i], rot_id_best_x[i])) return 1;
 	}
 
@@ -118,7 +162,8 @@ int test_optima_transformations()
 
 	// Rotate again by 90 degrees (plus normalize). We get back to the original problem (normalized_prob),
 	// but there is a twist here, because along the way the bounds were normalized several
-	// times (exactly four time). Therefore, all the vectors are scaled by the coef (1 / sqrt(2)) ** 4.
+	// times (exactly four time). Therefore, all the vectors are scaled by the factor (1 / sqrt(2)),
+	// except for the frist translation which is normalized by the factor 1 / 6.
 	// See src/problem/rotated.cpp for more details (function configure_new_bounds() in particular).
 	problem::rotated rotated_prob4(rotated_prob3,rot_mat90);
 	// Previous function rotated by 90 degrees counterclockwise and the minima are (after another normalization)
@@ -140,12 +185,34 @@ int test_optima_transformations()
 	for (unsigned int i = 0; i < expected_shrunk_minima.size(); ++i) {
 		if (!is_eq(expected_shrunk_minima[i], rot4_best_x[i])) return 1;
 	}
-	// -------------------------------------------------------------
+
+	return 0;
+}
+
+int test_normalized()
+{
+	// tak the original Himmelblau function
+	problem::himmelblau prob;
+	// and normalize it
+	problem::normalized normalized_prob(prob);
+	// Normalizing bounds (to [-1,1]x[-1,1]) of the Himmelblau function and the corresponding transformed minima are:
+	const std::vector<decision_vector> norm_best_x = normalized_prob.get_best_x();
+
+	const std::vector<decision_vector> expected_normalized_minima = {
+		{best_x[0][0] / 6, best_x[0][1] / 6},
+		{best_x[1][0] / 6, best_x[1][1] / 6},
+		{best_x[2][0] / 6, best_x[2][1] / 6},
+		{best_x[3][0] / 6, best_x[3][1] / 6},
+	};
+
+	for (unsigned int i = 0; i < norm_best_x.size(); ++i) {
+		if (!is_eq(expected_normalized_minima[i], norm_best_x[i])) return 1;
+	}
 
 	return 0;
 }
 
 int main()
 {
-	return test_optima_transformations();
+	return test_shifted() || test_rotated() || test_normalized();
 }
